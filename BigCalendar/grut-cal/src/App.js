e68c13fa8cf6f8de332f4f01cbd7803075fb9236
Sascha Reynolds <sreynolds@g.hmc.edu>
2018-10-17 15:08:41 -0700

    page with calendar and class list

--- BigCalendar/grut-cal/src/App.js
+++ BigCalendar/grut-cal/src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import './react-big-calendar.css';
+import { Column, Row } from 'simple-flexbox';
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
+
+const localizer = BigCalendar.momentLocalizer(moment)
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: ['CS81','CS70', 'CS121', 'CS105', 'CS60']
+    };
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+        <Row vertical='center'>
+          <Column flexGrow={1} horizontal='center'>
+            <text>Class List</text>
+            <CheckboxGroup
+              checkboxDepth={2} // This is needed to optimize the checkbox group
+              name="classes"
+              value={this.state.classes}
+              onChange={this.classesChanged}>
+
+              <label><Checkbox value="CS81"/> CS81</label>
+              <br></br>
+              <label><Checkbox value="CS70"/> CS70</label>
+              <br></br>
+              <label><Checkbox value="CS121"/> CS121</label>
+              <br></br>
+              <label><Checkbox value="CS105"/> CS105</label>
+              <br></br>
+              <label><Checkbox value="CS60"/> CS60</label>
+              
+            </CheckboxGroup>
+          </Column>
+          <Column flexGrow={1} horizontal='center'>
+              <BigCalendar
+              localizer={localizer}
+              events={[]}
+              startAccessor="startDate"
+              endAccessor="endDate"
+            />
+          </Column>
+        </Row>
+
+  );
 }
 
-export default App;
+classesChanged = (newClasses) => {
+  this.setState({
+    classes: newClasses
+  });
+}
+
+};
+
+export default App
